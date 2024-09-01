50f74b85023514a8510365780f802a4efe25a18f
JKannan17 <j.kannan@cognetiklabs.com>
2024-09-01 11:10:26 +0530

    Initial Commit

---
+++ ClinicBooking/ClinicBookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClinicBookingApp.swift
+//  ClinicBooking
+//
+//  Created by Janarthanan Kannan on 01/09/24.
+//
+
+import SwiftUI
+
+@main
+struct ClinicBookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
